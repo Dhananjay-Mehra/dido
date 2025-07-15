4996f0216ce2b64a7bfa6a72705cc4f0b68209fe
Dhananjay-Mehra <dhananjay7x.tumblr2353@gmail.com>
2025-07-15 18:06:53 +0530

    Update Download.jsx

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -42,7 +42,7 @@ function Download() {
 
               <div className="flex gap-2 w-full sm:w-auto">
                 <button className="border-2 py-2 px-4 sm:px-10 cursor-pointer w-full sm:w-auto">
-                  <a href="https://github.com/Krohit22/DIDO-virtual-voise-assistant-" target="_blank">Download</a>
+                  <a href="#" >Download</a>
                   
                 </button>
                 
